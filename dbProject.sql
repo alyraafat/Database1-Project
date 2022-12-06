60b41458364656cde8b8d62e5393946c5abfac1a
aly raafat <alewaraafat@gmail.com>
2022-12-07 01:29:09 +0200

    testing and solving queries from xiv -> xviii

--- dbProject.sql
+++ dbProject.sql
@@ -29,8 +29,8 @@ AS
 	);
 
 	CREATE TABLE StadiumManager(
-		username VARCHAR(20),
-		id INT IDENTITY,
+		username VARCHAR(20) UNIQUE,
+		id INT IDENTITY UNIQUE,
 		name VARCHAR(20),
 		stadium_id INT,
 		password VARCHAR(20),
@@ -40,8 +40,8 @@ AS
 	);
 
 	CREATE TABLE ClubRepresentative(
-		username VARCHAR(20),
-		id INT IDENTITY,
+		username VARCHAR(20) UNIQUE,
+		id INT IDENTITY UNIQUE,
 		name VARCHAR(20),
 		club_id INT,
 		password VARCHAR(20),
@@ -51,8 +51,8 @@ AS
 	);
 
 	CREATE TABLE Fan(
-		username VARCHAR(20),
-		national_id VARCHAR(20),
+		username VARCHAR(20) UNIQUE,
+		national_id VARCHAR(20) UNIQUE,
 		phone VARCHAR(20),
 		name VARCHAR(20),
 		address VARCHAR(20),
@@ -64,8 +64,8 @@ AS
 	);
 
 	CREATE TABLE SportsAssociationManager(
-		username VARCHAR(20),
-		id INT IDENTITY,
+		username VARCHAR(20) UNIQUE,
+		id INT IDENTITY UNIQUE,
 		name VARCHAR(20),
 		password VARCHAR(20),
 		CONSTRAINT pk_sam PRIMARY KEY(username,id),
@@ -73,8 +73,8 @@ AS
 	);
 
 	CREATE TABLE SystemAdmin(
-		username VARCHAR(20),
-		id INT IDENTITY,
+		username VARCHAR(20) UNIQUE,
+		id INT IDENTITY UNIQUE,
 		name VARCHAR(20),
 		password VARCHAR(20),
 		CONSTRAINT pk_system_admin PRIMARY KEY(username,id),
@@ -85,7 +85,7 @@ AS
 		id INT IDENTITY,
 		start_time DATETIME,
 		end_time DATETIME,
-		allowed_num_of_attendees INT,
+--		allowed_num_of_attendees INT,
 		stadium_id INT,
 		host_id INT,
 		guest_id INT,
@@ -115,6 +115,7 @@ AS
 		cru VARCHAR(20),
 		crd INT,
 		CONSTRAINT pk_host_request PRIMARY KEY(id),
+		CONSTRAINT check_status CHECK(status='accepted' OR status='rejected' OR status='unhandled'),
 		FOREIGN KEY(smu,smd) REFERENCES StadiumManager(username,id) ON DELETE CASCADE ON UPDATE CASCADE,
 		FOREIGN KEY(cru,crd) REFERENCES ClubRepresentative(username,id) ON DELETE CASCADE ON UPDATE CASCADE,
 		FOREIGN KEY(match_id) REFERENCES Matches(id) ON DELETE NO ACTION ON UPDATE NO ACTION
@@ -122,6 +123,92 @@ AS
 GO;
 Drop PROC createAllTables;
 EXEC createAllTables;
+
+
+--Test inserting into stadiums
+INSERT INTO Stadium (name,capacity,location) VALUES
+('camp nou',1000,'bar'),
+('santiago',2000,'madrid'),
+('2y neela',1000,'cairo');
+SELECT * FROM Stadium;
+
+--Test inserting into clubs
+INSERT INTO Club (name,location) VALUES
+('barca','bar'),
+('real madrid','madrid'),
+('ahly','cairo');
+SELECT * FROM Club;
+DELETE FROM CLUB
+
+--Test inserting into stadium manager
+--without stadium_id
+INSERT INTO StadiumManager (username,name,password) VALUES
+('alybaba','f','1234'),
+('xxx','c','4567'),
+('yyy','d','8900');
+
+--with stadium_id
+INSERT INTO StadiumManager (username,name,stadium_id,password) VALUES
+('ffff','g',2,'4324');
+SELECT * FROM StadiumManager;
+SET IDENTITY_INSERT StadiumManager ON;
+INSERT INTO StadiumManager (username,id,name,stadium_id,password) VALUES('',3,'',3,'')
+SET IDENTITY_INSERT StadiumManager OFF;
+DELETE FROM StadiumManager;
+
+--Test inserting into ClubRepresentative
+--without club_id
+INSERT INTO ClubRepresentative (username,name,password) VALUES
+('alybaba','f','1234'),
+('gggg','c','4567'),
+('hhhh','d','8900');
+
+--with club_id
+INSERT INTO ClubRepresentative (username,name,club_id,password) VALUES
+('ahly rep','7amaza',9,'4324'),
+('madrid rep','k',8,'4324'),
+('barca rep','sss',7,'4324');
+SELECT * FROM ClubRepresentative;
+DELETE FROM ClubRepresentative;
+
+--Test inserting into Fan
+INSERT INTO Fan (username,national_id,phone,name,address,birth_date,password) VALUES
+('f1','11111','01281002087','a','loc1','2002/10/30','3333'),
+('f2','22222','01281002086','b','loc2','2002/11/30','3334'),
+('f3','33333','01281002085','c','loc1','2002/10/30','3333'),
+('f4','44444','01281002085','c','loc1','2002/10/30','3333');
+SELECT * FROM Fan;
+DELETE FROM Fan;
+
+--Test inserting into SportsAssociationManager
+INSERT INTO SportsAssociationManager (username,name,password) VALUES
+('ddd','f','1234'),
+('fff','c','4567'),
+('gggg','d','8900');
+SELECT * FROM SportsAssociationManager;
+
+--Test inserting into Matches
+INSERT INTO Matches (start_time,end_time,stadium_id,host_id,guest_id) VALUES
+('2022/10/30 08:00:00','2022/10/30 10:00:00',2,8,7),
+('2022/10/28 05:00:00','2022/10/28 07:00:00',3,9,7),
+('2022/12/10 07:00:00','2022/12/10 09:00:00',1,7,8),
+('2022/12/11 07:00:00','2022/12/11 09:00:00',NULL,7,8);
+SELECT id ,CAST(start_time AS DATE) FROM Matches;
+SELECT * FROM MATCHES;
+DELETE FROM MATCHES
+DELETE FROM Club WHERE id=3;
+SELECT * FROM Club;
+
+--Test inserting into Host Request
+SELECT * FROM MATCHES;
+SELECT * FROM ClubRepresentative;
+SELECT * FROM StadiumManager;
+
+INSERT INTO HostRequest (match_id,smu,smd,cru,crd) VALUES
+(18,'fffffff','5','barca rep','11'),
+(15,'fffffff','5','madrid rep','10'),
+(16,'fffffff','5','ahly rep','9'),
+(17,'fffffff','5','barca rep','11');
 
 --2.1 b
 GO;
@@ -170,6 +257,10 @@ GO;
 CREATE VIEW allAssocManagers AS
 	SELECT S.username , S.name
 	FROM SportsAssociationManager S;
+GO;
+
+--Test allAssocManagers
+SELECT * FROM allAssocManagers;
 
 --2.2 b
 GO;
@@ -177,6 +268,12 @@ CREATE VIEW allClubRepresentatives AS
 	SELECT R.username , R.name AS club_rep_name, C.name AS club_name
 	From ClubRepresentative R
 		INNER JOIN Club C ON C.id = R.club_id
+GO;
+--Test allClubRepresentatives
+SELECT * FROM ClubRepresentative;
+SELECT * FROM Club;
+SELECT * FROM allClubRepresentatives;
+
 
 --2.2 c
 GO;
@@ -241,6 +338,9 @@ CREATE PROC addAssociationManager
 
 	INSERT INTO SportsAssociationManager(username, name, password) VALUES (@username , @name, @password);
 
+--Test addAssociationManager
+EXEC addAssociationManager @name = 'abbas', @username = 'zzz', @password='4343'
+SELECT * FROM SportsAssociationManager
 --2.3 ii
 GO;
 CREATE PROC addNewMatch
@@ -265,6 +365,10 @@ CREATE PROC addNewMatch
 	ELSE
 	INSERT INTO MATCHES (start_time , host_id , guest_id) VALUES (@time , @second , @first)
 
+-- Test addNewMatch
+--EXEC addNewMatch @nameFirstClub
+
+SELECT * FROM Matches;
 --2.3 iii
 GO;
 CREATE VIEW clubsWithNoMatches AS
@@ -442,5 +546,113 @@ CREATE PROC addRepresentative
 	WHERE C.name = @clubName
 
 	INSERT INTO ClubRepresentative (username,name,club_id,password) VALUES (@username,@repName,@clubId,@password);
+
+--2.3 xiv
+GO;
+CREATE FUNCTION viewAvailableStadiumsOn (@datetime DATETIME)
+	RETURNS TABLE
+	AS
+	RETURN
+		SELECT S.name,S.location,S.capacity
+		FROM Stadium S
+		WHERE S.status=1 AND NOT EXISTS(
+			SELECT * 
+			FROM Matches M
+			WHERE  S.id = M.stadium_id AND CAST(M.start_time AS DATE) = @datetime
+		)
+GO;
+--Test viewAvailableStadiumsOn
+SELECT * FROM Stadium;
+SELECT * FROM Matches;
+SELECt * FROM [dbo].viewAvailableStadiumsOn('2022/10/28');
+
+--2.3 xv
+GO;
+CREATE PROC addHostRequest 
+	@clubName VARCHAR(20),
+	@stadiumName VARCHAR(20),
+	@startTime DATETIME
+	AS 
+
+	DECLARE @clubRepId INT;
+	DECLARE @clubRepUsername VARCHAR(20);
+	DECLARE @hostId INT;
+	SELECT @clubRepId = CR.id, @clubRepUsername=CR.username, @hostId = C.id
+	FROM ClubRepresentative CR
+		INNER JOIN Club C on C.id = CR.club_id
+	WHERE C.name = @clubName
+
+	DECLARE @stadiumManagerId INT;
+	DECLARE @stadiumManagerUsername VARCHAR(20);
+	SELECT @stadiumManagerId = SM.id, @stadiumManagerUsername=SM.username
+	FROM StadiumManager SM
+		INNER JOIN Stadium S on S.id = SM.stadium_id
+	WHERE S.name = @stadiumName
+
+	DECLARE @matchId INT
+	SELECT @matchId = M.id
+	FROM Matches M
+	WHERE M.host_id = @hostId AND M.start_time = @startTime
+
+	INSERT INTO Ticket(match_id,smu,smd,cru,crd) VALUES (@matchId,@stadiumManagerUsername,@stadiumManagerId,@clubRepUsername,@clubRepId);
+GO;
+
+--2.3 xvi
+GO;
+CREATE FUNCTION allUnassignedMatches(@clubName VARCHAR(20))
+	RETURNS TABLE
+	AS 
+
+	RETURN
+		SELECT C2.name, M.start_time
+		FROM Matches M
+			INNER JOIN Club C1 ON C1.id = M.host_id
+			INNER JOIN Club C2 ON C2.id = M.guest_id
+		WHERE M.stadium_id IS NULL AND @clubName = C1.name
+GO;
+
+--Test allUnassignedMatches
+SELECT * FROM [dbo].allUnassignedMatches('barca');
+
+--2.3 xvii
+GO;
+CREATE PROC addStadiumManager
+	@name VARCHAR(20),
+	@stadiumName VARCHAR(20),
+	@username VARCHAR(20),
+	@password VARCHAR(20)
+	AS
+
+	DECLARE @stadiumId INT
+	SELECT @stadiumId = S.id
+	FROM Stadium S
+	WHERE S.name = @stadiumName
+
+	INSERT INTO StadiumManager(username,name,stadium_id,password) VALUES (@username,@name,@stadiumId,@password);
+GO;
+
+--Test addStadiumManager
+EXEC addStadiumManager @name='lol',@stadiumName='2y neela',@username='fffffff',@password='1234';
+SELECT * FROM Stadium;
+SELECT * FROM StadiumManager;
+
+--2.3 xviii
+GO;
+CREATE FUNCTION allPendingRequests (@username VARCHAR(20))
+	RETURNS TABLE
+	AS
+
+	RETURN 
+		SELECT CR.name AS club_rep_name, C.name AS competing_club,M.start_time
+		FROM HostRequest H
+			INNER JOIN StadiumManager SM ON H.smu=SM.username AND H.smd=SM.id
+			INNER JOIN ClubRepresentative CR ON H.cru = CR.username AND H.crd = CR.id
+			INNER JOIN Matches M ON M.id = H.match_id
+			INNER JOIN Club C ON C.id = M.guest_id
+		WHERE SM.username = @username AND H.status = 'unhandled'
+GO;
+
+--Test allPendingRequests
+SELECT * FROM [dbo].allPendingRequests('lol');
 
 
